e3d7a49e14df17e66094c34d20a0427c1eeaa3c4
akawsar <ali3kawsar6@gmail.com>
2026-09-06 13:10:47 +0600

    Add event listener to change text content on click

---
+++ class1/test.js
@@ -0,0 +1,10 @@
+let a=10
+console.log("Document")
+document.getElementById('submit')      
+
+  .addEventListener
+  ('click', function() { 
+    document.getElementById('change') 
+      .textContent = 'Content changed!';
+                       }
+  );
